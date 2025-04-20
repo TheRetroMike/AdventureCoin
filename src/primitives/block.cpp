a8f643fd0c19b37ee2a953b857899e23d786ed2c
Mihai Trif <84294388+ElektryonUK@users.noreply.github.com>
2025-04-20 21:45:12 +0100

    Update block.cpp

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -48,7 +48,7 @@ uint256 CBlockHeader::GetPoWHash() const
         .N = 2048, 
         .r = 32, 
         .pers = (const uint8_t *)"Let the quest begin",
-        .perslen = 20
+        .perslen = 19
     };
 
     uint32_t time = le32dec(&ss[68]);
